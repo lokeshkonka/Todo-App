4cdaf0b77c810a0bb5fa24ae0cfe66b34bfbbbe4
Konka Lokesh <konkalokesh372@gmail.com>
2025-06-11 17:35:46 +0530

    make functions

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-console.log("check");
+document.addEventListener('DOMContentLoaded',()=>{
+    console.log("check");
+
+const todoinput = document.getElementById("todo-input");
+
+const addtaskbutton = document.getElementById("add-task-btn");
+const tasklist = document.getElementById("todo-list");
+let tasks = JSON.parse(localStorage.getItem("tasks"))||[] ;
+tasks.forEach((task)=>rendertask(task));
+addtaskbutton.addEventListener('click',function(){
+    const tasktest = todoinput.value.trim()
+    if(tasktest === "") return;  // check whether the input is written or not
+    const newtask ={
+        id:Date.now,
+        text:tasktest,
+        completed:false
+    }// creates a new task , id for the to do
+    tasks.push(newtask);// adding to the tasks array
+    savetasks()
+    todoinput.value = "" // clear the value
+    console.log(tasks);
+    
+});
+function rendertask(t){
+ const li = document.createElement('li');
+ li.setAttribute("data-id",t.id)
+ li.innerHTML = `<span>${t.text}</span> <button>Delete</button>`
+ tasklist.appendChild(li)
+ li.addEventListener('click',(e)=>{
+    if(e.target.tagName === 'BUTTON' ) return;
+    t.completed = ! t.completed
+    li.classList.toggle('completed') //  takes boolean value
+    savetasks()
+  
+  
+ })
+}
+function savetasks(){
+    localStorage.setItem("tasks",JSON.stringify(tasks));
+}
+})
